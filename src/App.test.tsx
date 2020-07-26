5e3877b18353797684f937b67255f1255a60dda4
pi.wei <pi.wei@online-pajak.com>
2020-07-26 23:16:47 +0800

    feature: add input field and add-button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title of to-do app', () => {
+  render(<App />);
+  const header = screen.getByText(/to-do app/i);
+  expect(header).toBeInTheDocument();
+});
+
+test('renders to-do item added', () => {
+  render(<App />);
+  const textInput = screen.getByPlaceholderText("Add to-do");
+  const buttonAdd = screen.getByTestId("button-add");
+  const todoText = 'hello';
+  
+  fireEvent.change(textInput, { target: { value: todoText } });
+  fireEvent.click(buttonAdd);
+  const todoItemAdded = screen.getByText(todoText);
+  expect(todoItemAdded).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.scss';
+import React, { useState, SyntheticEvent } from "react";
+import ActionBar from './components/ActionBar/ActionBar';
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+const App = () => {
+	const [todoList, setTodoList] = useState<string[]>([]);
+  
+  const handleAddTodo = (todo: string) => {
+    const updatedTodoList = [...todoList, todo];
+		setTodoList(updatedTodoList);
+  }
+
+	return (
+		<div className="app">
+			<header>
+				<h1> To-Do App</h1>
+			</header>
+
+      <ActionBar onAddTodo={handleAddTodo} />
+
+      {todoList.map((todo, index) => (
+        <p key={index}>{todo}</p>
+      ))}
+		</div>
+	);
+};
 
 export default App;
